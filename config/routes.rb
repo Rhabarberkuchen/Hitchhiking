4cd9bc29e86141f764dd28889fd580e8d66578f4
Rhabarberkuchen <michaela.moes@gmx.net>
2018-02-20 14:10:48 +0100

    Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   get 'sessions/new'
 
-  resources :users
+  resources :users, :posts
   get 'basic_pages/profile'
 
   get 'basic_pages/home'
 
   get 'basic_pages/help'
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root 'application#hello'
