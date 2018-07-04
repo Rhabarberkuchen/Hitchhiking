c430e8d890fba7388c96eb65d9a08e0e5db4f4f9
Rhabarberkuchen <michaela.moes@gmx.net>
2018-07-04 17:57:16 +0200

    Update 2 after months

--- app/controllers/registrations_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationController < Devise::RegistrationController
-
-  private
-
-  #Modified Devise params for user login
-  def sign_up_params
-    params.require(:user).permit(:name, :birthday)
-  end
-end
