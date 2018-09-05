917fae8d233d817abd093899d9da252ee4f05030
Rhabarberkuchen <michaela.moes@gmx.net>
2018-09-05 12:01:38 +0200

    user.rb fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,26 @@
 class User < ApplicationRecord
-ApplicationRecord
+  #CarrierWave
+  mount_uploader :avatar, UserUploader
+  mount_uploader :pictures, PostPicturesUploader
+
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+  :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :friendships
+  has_many :friends, through: :friendships, dependent: :destroy
+  has_many :friend_requests
+
+  has_many :simple_posts
+  
   def was_friendship_requested_by?(other_user)
     requested = false
 
